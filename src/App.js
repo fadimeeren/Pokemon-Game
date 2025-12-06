c0f7ab77caf37fce9dc2271a9f5f49e632ad27cc
fadimeeren <fadime_ern135@outlook.com>
2025-12-06 18:03:25 +0300

    Updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import './Pokecard.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1> Pokemon Game</h1>
+    </div>
+  )
+}
+
+export default App
